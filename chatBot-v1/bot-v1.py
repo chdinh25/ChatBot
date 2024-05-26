a51c83df2cbfde3c8d79f6401165406878a8dc0b
Thanh Dinh <148485677+chdinh25@users.noreply.github.com>
2024-05-26 14:27:51 -0400

    Update bot-v1.py

--- chatBot-v1/bot-v1.py
+++ chatBot-v1/bot-v1.py
@@ -1,3 +1,8 @@
+# The code listens for spoken input from the user using a microphone, processes the speech to text using Google's speech recognition,
+# and responds with an appropriate message using text-to-speech. It can handle various commands such as greeting the user,
+# providing the current date and time, and responding to specific keywords. If the user says "bye," the bot will say "Good bye!"
+# and terminate the loop, ending the interaction.
+
 import pyttsx3
 import speech_recognition
 from datetime import date, datetime
@@ -9,15 +14,15 @@ speak = pyttsx3.init()
 # Variable to store the response of the chat bot
 thought = ""
 
-# Use while loop to keep the bot running and listening
+# Use a while loop to keep the bot running and listening
 while True:
     # Listening
     with speech_recognition.Microphone() as mic:
-        # Display a message of chat bot is working
+        # Display a message that the chat bot is working
         print("Robot: I'm listening")
         # Capturing the audio from the microphone
         audio = bot_ear.listen(mic)
-    # DIsplay a message show that the bot is processing the speech
+    # DIsplay a message showing that the bot is processing the speech
     print("Robot: ...")
     # Use try to catch any exceptions
     try:
@@ -33,22 +38,22 @@ while True:
         # If nothing was heard, prompt the user to try again
         thought = "I can't hear you, please try again"
     elif "hello" in you:
-        # Respond to message contain the word "hello"
+        # Respond to a message containing the word "hello"
         thought = "Hello"
     elif "today" in you:
-        # Respond to message contain the word "today"
+        # Respond to a message containing the word "today"
         # Get today's date
         today = date.today()
         # Format the date in a readable way and store it in variable thought
         thought = today.strftime("%B %d, %Y")
     elif "time" in you:
-        # Respond to message contain the word "time"
+        # Respond to message containing the word "time"
         # Get current time
         now = datetime.now()
         # Format the time and store it in variable thought
         thought = now.strftime("%H hours %M minutes %S second")
     elif "president" in you:
-        # Respond to message contain the word "president"
+        # Respond to messages contain the word "president"
         thought = "Joe Biden"
     elif "bye" in you:
         # Respond to "bye" and break the loop to stop the bot
